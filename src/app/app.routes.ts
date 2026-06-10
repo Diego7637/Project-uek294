7616a3f9da2f399d4b9a33983f3dbf2a073f96c9
Diego7637 <diego.chervet@ict.csbe.ch>
2026-06-10 14:12:23 +0200

    Register base Products page created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home';
+import { LoginComponent } from './pages/login/login';
+import { ProductsComponent } from './pages/products/products';
+import { RegisterComponent } from './pages/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'register', component: RegisterComponent }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header';
 
 @Component({
-  selector: 'pm-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: './app.html'
 })
-export class App {
-  protected readonly title = signal('Project-uek294');
-}
+export class AppComponent {}
